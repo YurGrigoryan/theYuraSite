ca7a761238b86ba006a6e8f3fbce074de0b09558
YurGrigoryan <yurag5133@gmail.com>
2022-11-28 16:10:25 +0400

    hyCommit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let w = document.querySelector(".SmallDivGame")
 let o = document.querySelector(".startH2")
 let p = document.querySelector(".h3")
 let q = document.getElementById("hEnd")
+let a = document.getElementById("hEnd2")
 
 x.addEventListener("click" , function(){
     h2.innerText = "Իմ անունը Յուրա է։ Ես Հայաստանից եմ։Ես սովորում եմ ծրագրավորում 6 ամիս։Այս ամիսների ընթացքում ես շատ բաներ սովորեցի։Իմ երազանքն է դառնալ ամենալավ ծրագրավորողներից մեկը աշխարհում։Ես շատ եմ սիրում խաղեր։Այդ իսկ պատճառով ես որոշեցի որ պետք է ծրագրավորող դառնամ։Ես այս մի քանի ամսում մի քանի կայքեր եմ պատրաստել որոնցից վերջինը այս կայքն է։Ինձ շատ է դուր գալիս ծրագրավորումը և ես սովորում եմ որպեսզի դառնամ լավ ծրագրավորող։"
@@ -17,6 +18,7 @@ x.addEventListener("click" , function(){
     o.innerText = "Սա նույնպես իմ պատրաստած խաղերից մեկն է, որը ես գնահատում եմ 3$"
     p.innerText = "իմ github-ը"
     q.innerText = "Իսկ սա իմ պատրաստած կայքերից մեկն է"
+    a.innerText = "Սա նույնպես իմ պատրաստած կայքերից մեկն է"
 })
 y.addEventListener("click" , function(){
     h2.innerText = "Меня зовут Юра. Я из Армении.Я изучаю программирование уже 6 месяцев.За эти месяцы я многому научился.Мечтаю стать одним из лучших программистов в мире.Я очень люблю игры.Поэтому я решил, что я должен стать программистом.За эти несколько месяцев я сделал несколько сайтов,последним из которых является этот сайт.Мне очень нравится программировать и я учусь стать хорошим программистом. "
@@ -25,6 +27,7 @@ y.addEventListener("click" , function(){
     o.innerText = "Это также одна из моих игр, которую я оцениваю в 3 доллара."
     p.innerText = "мой github"
     q.innerText = "И это один из сайтов, которые я сделал"
+    a.innerText = "Это также один из сайтов, которые я сделал"
 })
 z.addEventListener("click" , function(){
     h2.innerText = "My name is Yura. I am from Armenia. I have been studying programming for 6 months. I have learned many things during these months. My dream is to become one of the best programmers in the world. I love games very much. That's why I decided that I should become a programmer. In these few months, I I have made several websites, the last of which is this website. I really like programming and I am learning to become a good programmer."
@@ -33,5 +36,6 @@ z.addEventListener("click" , function(){
     o.innerText = "This is also one of the games. I made that I value at $3"
     p.innerText = "my github"
     q.innerText = "And this is one of the sites I made"
+    a.innerText = "This is also one of the sites I made"
 })
 
